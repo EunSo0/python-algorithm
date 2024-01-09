66525831a0047cae9c511059fd11d484feec37a4
Eunsoo <101039358+EunSo0@users.noreply.github.com>
2024-01-09 13:47:28 +0900

    Time: 235 ms (69.50%), Space: 20.8 MB (14.92%) - LeetHub

--- 0561-array-partition/0561-array-partition.py
+++ 0561-array-partition/0561-array-partition.py
@@ -4,10 +4,8 @@ class Solution:
         pair = []
         nums.sort()
         
-        for n in nums:
-            pair.append(n)
-            if len(pair) == 2:
-                sum += min(pair)
-                pair = []
+        for i, n in enumerate(nums):
+            if i % 2 == 0:
+                sum += n
                 
         return sum
