b2a07e90734d7591a1974e82b2c5fe19f8b5c690
Eunsoo <101039358+EunSo0@users.noreply.github.com>
2024-01-10 16:10:30 +0900

    Time: 731 ms (6.86%), Space: 38.2 MB (67.22%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -2,18 +2,15 @@ class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
         list = []
         
+        if not head:
+            return True
+        
         while head:
             list.append(head.val)
             head = head.next
-        
-        left = 0
-        right = len(list)-1
-        
-        while left <= right:
-            if list[left] == list[right]:
-                left += 1
-                right -= 1
-            else: 
+
+        while len(list) > 1:
+            if list.pop(0) != list.pop():
                 return False
         
         return True
