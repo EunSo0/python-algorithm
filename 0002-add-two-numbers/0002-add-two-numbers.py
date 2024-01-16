b8957b27359aca6d151b46eedb8e24201e0ed2c9
Eunsoo <101039358+EunSo0@users.noreply.github.com>
2024-01-16 15:44:36 +0900

    Time: 70 ms (28.69%), Space: 17.3 MB (25.51%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -1,30 +1,40 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
 class Solution:
+    def reverseList(self, head:ListNode) -> ListNode:
+        node, prev = head, None
+        
+        while node:
+            next = node.next
+            node.next = prev
+            prev = node
+            node = next
+            
+        return prev
+    
+    def toList(self, node:ListNode) -> List:
+        list: List = []
+        
+        while node:
+            list.append(node.val)
+            node = node.next
+        return list
+    
+    def toReversedLinkedList(self, result: str) -> ListNode:
+        prev = None
+        for r in result:
+            node = ListNode(r)
+            node.next = prev
+            prev = node
+            
+        return node
+        
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        resultNode = ListNode()
-        num1, num2 ='',''
+        a = self.toList(self.reverseList(l1))
+        b = self.toList(self.reverseList(l2))
         
-        while l1:
-            num1 += str(l1.val)
-            l1 = l1.next
-        while l2:
-            num2 += str(l2.val)
-            l2 = l2.next
-        result = str(int(num1[::-1]) + int(num2[::-1]))
+        resultStr = int(''.join(str(e) for e in a)) + \
+                    int(''.join(str(e) for e in b))
         
-        curr = resultNode
-        for re in result[::-1]:
-            curr.next = ListNode()
-            curr = curr.next
-            curr.val = int(re)
+        return self.toReversedLinkedList(str(resultStr))
+
             
-            
-        return resultNode.next
-        
-        
-        
             
