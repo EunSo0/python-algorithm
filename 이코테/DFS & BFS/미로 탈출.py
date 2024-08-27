df0965f6d0168d2036e4f13b4073d969e3497292
EunSo0 <dmstn423@naver.com>
2024-08-27 14:39:37 +0900

    add: 미로 탈출

---
+++ 이코테/DFS & BFS/미로 탈출.py
@@ -0,0 +1,30 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+n,m = map(int,input().split())
+graph = []
+for i in range(n):
+    graph.append(list(map(int,input().strip())))
+
+dx = [0,0,-1,1]
+dy = [1,-1,0,0]
+
+def bfs(x,y):
+    queue = deque()
+    queue.append((x,y))
+
+    while queue:
+        x,y = queue.popleft()
+
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if 0 <= nx < n and 0 <= ny < m:
+                if graph[nx][ny] == 1:
+                    graph[nx][ny] = graph[x][y] + 1
+                    queue.append((nx,ny))
+    return graph[n-1][m-1]
+
+print(bfs(0,0))
